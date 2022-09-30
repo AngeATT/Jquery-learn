0ec2e03ffc7abdf4642d3dd66120826c817f84cd
AngeATT <96740251+AngeATT@users.noreply.github.com>
2022-09-30 14:39:00 +0000

    Introduction gestion d'évènement
    event handler avec on(event, fonciton)

--- scriptJQuery.js
+++ scriptJQuery.js
@@ -1,1 +1,9 @@
-$('p').hide();
+$(function(){ // pour dire qu'on attends le fin de chargement du DOM pour commencer
+
+    $('p').on('click',
+        function(){
+            $(this).hide();
+        }
+    )
+
+});

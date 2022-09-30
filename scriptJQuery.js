571a722e534cf7d273899617dbd612ec2db8bae6
AngeATT <96740251+AngeATT@users.noreply.github.com>
2022-09-30 15:12:35 +0000

    mouseenter et mouseleave
    Effet slow quand on a la mouse dessus et quand on quitte l'élément

--- scriptJQuery.js
+++ scriptJQuery.js
@@ -1,9 +1,18 @@
 $(function(){ // pour dire qu'on attends le fin de chargement du DOM pour commencer
 
-    $('p').on('click',
+    /*$('p').on('mouseenter',
         function(){
-            $(this).hide();
+            $(this).hide("slow");
         }
-    )
+    )*/ // cache les paragraphes clickés on peut utiliser dbclick etc
+    
+    $('p').on('mouseenter', function(){
+        $(this).hide("slow");
+    }).on('mouseleave', 
+    function(){
+        $(this).show("slow");
+    });
+
+
 
 });
